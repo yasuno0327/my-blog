4649a73f62936c49679703a6ef7d4f0dd1ce4431
yasuno0327 <5315076@ed.tus.ac.jp>
2018-11-02 23:14:49 +0900

    Implement generate token function

--- api/service/session_manager.go
+++ api/service/session_manager.go
@@ -1,6 +1,10 @@
 package service
 
 import (
+	"io/ioutil"
+	"log"
+	"time"
+
 	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
@@ -20,9 +24,33 @@ func SessionClear(context interface{}) {
 	session.Save()
 }
 
-func GenerateJWT(email string) {
+// GenerateJWT Generate JWT Token
+func GenerateJWT(email string) string {
+	// Read private key
+	signByte, err := readSecretByte()
+	if err != nil {
+		log.Fatal(err)
+	}
+	signKey, err := jwt.ParseECPrivateKeyFromPEM(signByte)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"email": email,
-		"expired_date": time.Date(2018, time.December)
+		"email":        email,
+		"expired_date": time.Date(2018, time.December, 1, 0, 0, 0, 0, time.UTC).Unix(),
 	})
+	tokenString, err := token.SignedString(signKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return tokenString
+}
+
+func readSecretByte() ([]byte, error) {
+	signBytes, err := ioutil.ReadFile("/go/src/api/me.rsa")
+	if err != nil {
+		return nil, err
+	}
+	return signBytes, nil
 }
