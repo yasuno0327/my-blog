85099eebd50d9b508536b135c37c331a07678fef
yasuno0327 <5315076@ed.tus.ac.jp>
2019-07-12 11:02:49 +0900

    remove unused module

--- front/src/containers/HomeContainer.js
+++ front/src/containers/HomeContainer.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { Home } from '../components'
 import * as HomeActions from '../actions/home'
 import { createStructuredSelector, createSelector } from 'reselect'
--- front/src/containers/SigninContainer.js
+++ front/src/containers/SigninContainer.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { SignIn } from '../components'
 import * as SignInActions from '../actions/signin'
 import { createStructuredSelector, createSelector } from 'reselect'
